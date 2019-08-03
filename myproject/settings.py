8677085aef259f9e2914ad6568286d6aca819ab7
batTrung <trungbat.pv@gmail.com>
2019-08-03 17:07:11 +0700

    add

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
