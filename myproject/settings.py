70f99a23ac33f227d1cd19cab3553bafcab1d7f6
batTrung <trungbat.pv@gmail.com>
2019-08-03 13:18:43 +0700

    add djang-hreoku

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -149,3 +151,6 @@ LOGOUT_URL = 'logout'
 
 from django.contrib.messages import constants as message_constants
 MESSAGE_LEVEL = message_constants.DEBUG
+
+
+django_heroku.settings(locals())
