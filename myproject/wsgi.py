bd290e6a4c71d7971069d5421e99e0b5eb5ebbca
batTrung <trungbat.pv@gmail.com>
2019-08-03 19:04:05 +0700

    update NAVBAR

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
-
-
+application = DjangoWhiteNoise(application)
