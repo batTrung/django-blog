1d2fb9330534903ee13e625bd4b05675f96958ff
batTrung <trungbat.pv@gmail.com>
2019-08-03 19:13:37 +0700

    update NAVBAR

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -8,13 +8,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-
-
-from django.core.wsgi import get_wsgi_application
-
 
 application = get_wsgi_application()
 
