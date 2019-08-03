ba363a765ae3b8cff89c3f2bd49fefc033ca747e
batTrung <trungbat.pv@gmail.com>
2019-08-03 19:45:48 +0700

    add

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qwksaffus6_gz7$sr@skf8e$zc9um8r@%7-2o3rr_d9r14#!1j'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'qwksaffus6_gz7$sr@skf8e$zc9um8r@%7-2o3rr_d9r14#!1j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
