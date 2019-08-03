5b537213c363f8f4666a2282ade7a82ac5cb7926
batTrung <trungbat.pv@gmail.com>
2019-08-03 21:52:37 +0700

    add

--- myproject/settings.py
+++ myproject/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = config('STATICFILES_STORAGE')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
